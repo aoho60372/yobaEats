674aed1470804a357da82f8ea5caf515677fe8e9
unknown <kolochenkov90@gmail.com>
2022-08-18 20:03:15 +1000

    update

---
+++ scripts/game.js
@@ -0,0 +1,242 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+var textField = document.querySelector("h1");
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+var eatedCount = 0;
+var kolobokStartSize = 100;
+var animCycle = 400;
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random RGB color value
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+function drawBall(e) {
+    var cursorX = e.pageX;
+    var cursorY = e.pageY;
+
+    do {
+        var velX = random(-7, 7);
+        var velY = random(-7, 7);
+    } while (velY === velX === 0);
+
+    var ball = new Ball(cursorX, cursorY, velX, velY, randomRGB(), random(10, 20));
+    balls.push(ball);
+}
+
+class Ball {
+
+    constructor(x, y, velX, velY, color, size){
+        if (x + size >= width){
+            this.x = x - size;
+        } else if (x - size <= 0) {
+            this.x = x + size;
+        } else {
+            this.x = x;
+        }
+
+        if (y + size >= height) {
+            this.y = y - size;
+        } else if (y - size <= 0) {
+            this.y = y + size;
+        } else {
+            this.y = y;
+        }
+
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+        this.exist = 1;
+        this.cooldown = 0;
+        this.animCycle = 0;
+        this.updateCycle = 0;
+    }
+
+    draw() {
+        if (this.exist === 1){
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+    }
+
+    update() {
+        
+        if (this.x + this.size >= width){
+            this.velX = -(this.velX);
+            this.x -= this.size;
+        }
+        if (this.x - this.size <= 0){
+            this.velX = -(this.velX);
+            this.x += this.size;
+        }
+        if (this.y + this.size >= height){
+            this.velY = -(this.velY);
+            this.y -= this.size;
+        }
+        if (this.y - this.size <= 0){
+            this.velY = -(this.velY);
+            this.y += this.size;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+
+    }
+
+    collisionDetect() {
+        for (var ball of balls){
+            if (!(this === ball)){
+                    if (!(Kolobok.prototype.isPrototypeOf(ball))){
+                        var distance = Math.sqrt(Math.pow(this.x - ball.x, 2) + Math.pow(this.y - ball.y, 2)); // гуглить расстояние между двумя точками
+                        if (distance < (this.size + ball.size)){
+                            this.velX = -(this.velX);
+                            this.velY = -(this.velY);
+                            do {
+                                ball.velX = random(-7, 7);
+                                ball.velY = random(-7, 7);
+                            } while (ball.velY === ball.velX === 0);
+                        }
+                    }  
+            }
+        }
+    }
+}
+
+class Kolobok extends Ball {
+
+    draw(){
+        ctx.drawImage(this.color, this.x, this.y, this.size, this.size);
+        this.birth();
+        if (this.updateCycle >= 20 && this.animCycle === 0) {
+            this.color = imgKolobok;
+            this.updateCycle = 0;
+        }
+    }
+
+    update() {
+        if (this.x + this.size >= width){
+            this.velX = -(this.velX);
+            this.x -= this.size / 30;
+        }
+        if (this.x <= 0){
+            this.velX = -(this.velX);
+            this.x += this.size / 30;
+        }
+        if (this.y + this.size >= height){
+            this.velY = -(this.velY);
+            this.y -= this.size / 30;
+        }
+        if (this.y <= 0){
+            this.velY = -(this.velY);
+            this.y += this.size / 30;
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+        this.updateCycle++;
+    }
+
+    collisionDetect() {
+        for (var ball of balls){
+            if (!(Kolobok.prototype.isPrototypeOf(ball))){
+                if (!(ball.velX === ball.velY === 0)){
+                    var distance = Math.sqrt(Math.pow((this.x + (this.size / 2)) - ball.x, 2) + Math.pow((this.y + (this.size / 2)) - ball.y, 2)); // гуглить расстояние между двумя точками
+                    if (distance < ((this.size / 3) + ball.size)){
+                        let imgKolobokEats = new Image();
+                        imgKolobokEats.src = 'images/kolobok-eats.png';
+                        this.color = imgKolobokEats;
+    
+                        ball.exist = 0;
+                        eatedCount++;
+                        this.updateCycle++;
+                        textField.textContent = "Ехидный колобок слопал: " + eatedCount;
+                        this.size++;
+    
+                        if (this.cooldown === 5){
+                            this.velX = random(-7, 7);
+                            this.velY = random(-7, 7);
+                            this.cooldown = 0;
+                        }
+                        
+                        balls.splice(balls.indexOf(ball), 1);
+                        this.cooldown++;
+                    }
+                }   
+            }
+        }
+    }
+
+    birth() {
+        if ((this.size >= (width * 0.3)) || (this.size >= (height * 0.3))){
+            for (var ball of balls){
+                ball.velX = ball.velY = 0;
+                if (this === ball){
+                    if (this.animCycle === 0){
+                        canvas.removeEventListener('click', drawBall);
+                        let imgKolobokBad = new Image();
+                        imgKolobokBad.src = 'images/kolobok-bad.png';
+                        this.color = imgKolobokBad;
+                    }
+                    if (this.animCycle < animCycle){
+                        this.velX = random(-1, 1);
+                        this.velY = random(-1, 1);
+                        this.animCycle++;
+                    }
+                    if (this.animCycle >= animCycle){
+                        this.color = imgKolobok;
+                        this.size = kolobokStartSize;
+                    }
+                }
+            }
+            if (this.animCycle >= animCycle){
+                for (var ball of balls){
+                    ball.velX = random(-7, 7);
+                    ball.velY = random(-7, 7);
+                    this.animCycle = 0;
+                }
+                balls.unshift(new Kolobok(this.x + this.size, this.y, random(-7, 7), random(-7, 7), imgKolobok, kolobokStartSize));
+                canvas.addEventListener('click', drawBall);
+            }
+        } else return;
+    }
+}
+
+var balls = [];
+
+var imgKolobok = new Image();
+imgKolobok.src = 'images/kolobok.png';
+
+imgKolobok.onload = function() {
+    balls.push(new Kolobok(width / 2, height / 2, random(-7, 7), random(-7, 7), imgKolobok, kolobokStartSize));
+
+    function loop() {
+        width = canvas.width = window.innerWidth;
+        height = canvas.height = window.innerHeight;
+
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.6)';
+        ctx.fillRect(0, 0,  width, height);
+
+        for (var ball of balls){
+            ball.draw();
+            ball.update();
+            ball.collisionDetect();
+        }
+
+        requestAnimationFrame(loop);
+    }
+    canvas.addEventListener('click', drawBall);
+    loop();
+}
